f655ee149118f30eaa26601593b045d1b579c83f
gzepeda09 <gzepeda09@gmail.com>
2022-10-28 20:31:54 -0700

    my first commit

---
+++ test.py
@@ -0,0 +1,47 @@
+
+""" 
+Create a Disease
+mortality rate: 20%
+lifetime of the virus: 2 days
+duration of the infection: 1 week
+transmission rate: 70%
+"""
+
+import random
+import time
+import os
+
+os.system('clear')
+while 1:
+    num = random.randrange(1,20,1)
+    if num < 2:
+        print(num)
+        break
+    else:
+        print(num)
+    time.sleep(0.35)
+    os.system('clear')
+exit()
+
+"""
+mortality rate: 20%
+lifetime of the virus: 2 days
+duration of the infection: 1 week
+transmission rate: 70%
+
+import random
+import time
+import os
+
+os.system('clear')
+while 1:
+    num = random.randrange(1,20,1)
+    if num < 2:
+        print(num)
+        break
+    else:
+        print(num)
+    time.sleep(0.35)
+    os.system('clear')
+exit()
+"""
