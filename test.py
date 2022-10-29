103a66eaf09d4d6c31524fab1776f4257e80cc05
gzepeda09 <gzepeda09@gmail.com>
2022-10-28 21:12:17 -0700

    update

--- test.py
+++ test.py
@@ -8,40 +8,20 @@ transmission rate: 70%
 """
 
 import random
-import time
-import os
 
-os.system('clear')
-while 1:
-    num = random.randrange(1,20,1)
-    if num < 2:
-        print(num)
-        break
-    else:
-        print(num)
-    time.sleep(0.35)
-    os.system('clear')
+passenger = random.randrange(300,350,1)
+mrate = 0.20
+ltime: 2
+dinf = 7
+trate = 0.70
+  
+# Creating a name list
+name_lst = ['350', '275', '312',
+            '200', '150', '320']
+  
+print(random.choices(name_lst, weights=(
+    70, 70, 70, 70, 70, 70), k=1))
+
+
 exit()
 
-"""
-mortality rate: 20%
-lifetime of the virus: 2 days
-duration of the infection: 1 week
-transmission rate: 70%
-
-import random
-import time
-import os
-
-os.system('clear')
-while 1:
-    num = random.randrange(1,20,1)
-    if num < 2:
-        print(num)
-        break
-    else:
-        print(num)
-    time.sleep(0.35)
-    os.system('clear')
-exit()
-"""
